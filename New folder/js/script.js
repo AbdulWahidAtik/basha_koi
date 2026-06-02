b64bcd6450a442c7abe86d64857ac73fd94365c3
Atik Wahid <wahidabdul50084@gmail.com>
2026-06-02 11:28:43 +0600

    Add .gitignore and remove system/cache files

--- New folder/js/script.js
+++ New folder/js/script.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const forms = document.querySelector(".forms"),
       pwShowHide = document.querySelectorAll(".eye-icon"),
       links = document.querySelectorAll(".link");
@@ -24,4 +25,32 @@ links.forEach(link => {
        e.preventDefault(); //preventing form submit
        forms.classList.toggle("show-signup");
     })
+=======
+const forms = document.querySelector(".forms"),
+      pwShowHide = document.querySelectorAll(".eye-icon"),
+      links = document.querySelectorAll(".link");
+
+pwShowHide.forEach(eyeIcon => {
+    eyeIcon.addEventListener("click", () => {
+        let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
+        
+        pwFields.forEach(password => {
+            if(password.type === "password"){
+                password.type = "text";
+                eyeIcon.classList.replace("bx-hide", "bx-show");
+                return;
+            }
+            password.type = "password";
+            eyeIcon.classList.replace("bx-show", "bx-hide");
+        })
+        
+    })
+})      
+
+links.forEach(link => {
+    link.addEventListener("click", e => {
+       e.preventDefault(); //preventing form submit
+       forms.classList.toggle("show-signup");
+    })
+>>>>>>> ec5b284 (Initial commit after username change)
 })
